9b481917bfa31174edca7b164dfbabda6c2136a6
isaevanazik6 <isaevanazik6@gmail.com>
2025-11-30 23:11:34 +0600

    add Vue js default file + main.ts

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Navbar from "@/components/layout/Navbar.vue";
+import Footer from "@/components/layout/Footer.vue";
+import {onMounted} from "vue";
+import {useContactsStore} from "@/stores/contacts-store.ts";
+
+const contactsStore = useContactsStore();
+
+onMounted(() => {
+  contactsStore.load();
+});
+</script>
+
+<template>
+  <Navbar />
+  <main class="min-h-screen bg-[#faf7f4]">
+    <router-view />
+  </main>
+  <Footer />
+</template>
